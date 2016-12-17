01ab7b9615fe8ef4b5174c20430fa4c1d775f107
Adam Larsen <adam@collabip.com>
2016-12-16 20:15:20 -0600

    Added NuGet Spec file

--- src/Tethr.AudioBroker/Properties/AssemblyInfo.cs
+++ src/Tethr.AudioBroker/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tethr™ Audio Broker SDK")]
+[assembly: AssemblyTitle("Tethr® Audio Broker SDK")]
 [assembly: AssemblyDescription("SDK for sending audio and metadata for a call to Tethr.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CollabIP, inc.")]
-[assembly: AssemblyProduct("Tethr™")]
+[assembly: AssemblyProduct("Tethr®")]
 [assembly: AssemblyCopyright("Copyright © CollabIP, inc. 2016")]
 [assembly: AssemblyTrademark("Tethr is a trademark of CollabIP, inc. CollabIP is a trademark of CollabIP, inc.")]
 [assembly: AssemblyCulture("")]
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
