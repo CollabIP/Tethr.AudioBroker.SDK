ada0d39a084a9a9ca46cfdb1abfcfa498c589bd2
Adam Larsen <adam@collabip.com>
2017-05-26 17:31:24 -0500

    Updated SDK with Heartbeat API
    Also included a sample timer to send heartbeats

---
+++ src/Tethr.AudioBroker/TethrHeartbeat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tethr.AudioBroker.Model;
+using Tethr.AudioBroker.Session;
+
+namespace Tethr.AudioBroker
+{
+	public interface ITethrHeartbeat
+	{
+		Task Send(MonitorEvent monitorEvent);
+	}
+
+	public static class TethrHeartbeatExtensions
+	{
+		public static async Task Send(this ITethrHeartbeat heartbeat, MonitorStatus monitorStatus)
+		{
+			await heartbeat.Send(new MonitorEvent
+			{
+				Name = Environment.MachineName,
+				Status = monitorStatus,
+				TimeStamp = DateTimeOffset.UtcNow
+			});
+		}
+	}
+
+	/// <summary>
+	/// Used to send Tethr a heartbeat from a broker.  Allowing you to then monitor if a given broker is able to correctly send data to Tethr at all times.
+	/// </summary>
+	/// <remarks>
+	/// An endpoint can be made available in Tethr to allow you to connect up your own monitoring or alerting system to see that status of a given broker.
+	/// This is used to give a high level alert of any possible issue that may prevent a broker from uploading calls to Tethr.
+	/// </remarks>
+	public class TethrHeartbeat : ITethrHeartbeat
+	{
+		private readonly ITethrSession _tethrSession;
+
+		public TethrHeartbeat(ITethrSession tethrSession)
+		{
+			_tethrSession = tethrSession;
+		}
+
+		public async Task Send(MonitorEvent monitorEvent)
+		{
+			await _tethrSession.PostAsync(@"callCapture/v1/monitor", monitorEvent);
+		}
+	}
+}
---
+++ src/Tethr.UploadRecordingSample/TethrHeartbeatTimerSample.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Common.Logging;
+using Tethr.AudioBroker;
+using Tethr.AudioBroker.Model;
+
+namespace Tethr.UploadRecordingSample
+{
+	/// <summary>
+	/// A sample of a timer that can send a heart beat update to Tethr at a set interval.
+	/// </summary>
+	/// <remarks>
+	/// This would be a singleton that is created during application startup.
+	/// </remarks>
+	public sealed class TethrHeartbeatTimerSample : IDisposable
+	{
+		private const uint Period = 60000; // send every 60 seconds
+
+		private readonly ITethrHeartbeat _heartbeat;
+
+		private readonly Timer _internalTimer;
+		private readonly ILog _log = LogManager.GetLogger(typeof(TethrHeartbeatTimerSample));
+		private readonly object _syncRoot = new object();
+		private bool _disposed;
+
+		private Func<MonitorStatus> _getStatusCallback;
+
+		public TethrHeartbeatTimerSample(ITethrHeartbeat heartbeat)
+		{
+			_heartbeat = heartbeat;
+			// Using due time and not period in timer so that if our call to Tethr
+			// takes longer then then the period, we don't try to send two at a time.
+			_internalTimer = new Timer(Callback, null, Period, 0);
+		}
+
+		/// <summary>
+		/// Setup a call back for checking the status health of the broker.
+		/// </summary>
+		/// <remarks>
+		/// This allows for the broker to check other internal status indicators to see if the system is health.
+		/// If a call back is not set, a default value of Healthy will be sent to Tethr.
+		/// </remarks>
+		/// <param name="getStatusCallback">A function that will return the current status of the broker</param>
+		public void SetupCallBack(Func<MonitorStatus> getStatusCallback)
+		{
+			if (_getStatusCallback != null)
+				throw new InvalidOperationException("Call back already set.");
+			_getStatusCallback = getStatusCallback;
+		}
+
+		private void Callback(object state)
+		{
+			if (_disposed)
+				return;
+
+			lock (_syncRoot)
+			{
+				try
+				{
+					var status = MonitorStatus.Healthly;
+					var cb = _getStatusCallback;
+					if (cb != null)
+						status = cb();
+
+					// Spin up in a new Task so we have a full Synchronization context
+					// If we don't do this, it's possible for this thread to get stuck.
+					Task.Run(() => _heartbeat.Send(status)).GetAwaiter().GetResult();
+					Online = true;
+				}
+				catch (Exception e)
+				{
+					// Because timer call backs are on their own threads,
+					// we must capture all exception, or it will result
+					// in an unhandled exception and the process will terminate.
+
+					if (Online)
+					{
+						// State is transitioning, we will log as an error
+						_log.Error("Error sending heart beat to Tethr.  " + e.Message, e);
+						Online = false;
+					}
+					else if (_log.IsTraceEnabled)
+					{
+						// We don't want to flood the logs with errors, so we will just send it as a trace.
+						_log.Trace("Error sending heart beat to Tethr.  " + e.Message, e);
+					}
+				}
+				finally
+				{
+					if (!_disposed)
+						_internalTimer.Change(Period, 0);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Indicates that our connection to Tethr is online, and we can send call to Tethr.
+		/// </summary>
+		public bool Online { get; private set; }
+
+		~TethrHeartbeatTimerSample()
+		{
+			Dispose(false);
+		}
+
+		public void Dispose()
+		{
+			lock (_syncRoot)
+			{
+				Dispose(true);
+			}
+		}
+
+		private void Dispose(bool disposing)
+		{
+			_disposed = true;
+			if (disposing)
+			{
+				WaitHandle wh = new ManualResetEvent(false);
+				_internalTimer.Dispose(wh);
+				wh.WaitOne();
+			}
+		}
+	}
+}
