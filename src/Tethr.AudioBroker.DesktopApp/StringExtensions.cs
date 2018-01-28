a0a95e0e6a8179e9a17bab7ed5720d8a561b3952
Adam Larsen <Adam@collabip.com>
2018-01-28 02:35:19 -0600

    connected desktop client to Tethr

---
+++ src/Tethr.AudioBroker.DesktopApp/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Tethr.AudioBroker.DesktopApp
+{
+    internal static class StringExtensions
+    {
+	    public static string NullIfEmpty(this string value)
+	    {
+		    return string.IsNullOrEmpty(value) ? null : value;
+	    }
+
+	    public static string TextValueOrNull(this TextBox value)
+	    {
+		    return string.IsNullOrEmpty(value.Text) ? null : value.Text;
+	    }
+    }
+}
