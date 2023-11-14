7886f9a296a87f86e1720be8d7122718c518cf4f
ChazRowe <chaz@collabip.com>
2023-11-13 19:55:35 -0500

    Add case support.

---
+++ src/Tethr.AudioBroker/Model/CaseResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace Tethr.AudioBroker.Model
+{
+    /// <summary>
+    /// Response when creating or updating a case.
+    /// </summary>
+    public class CaseResponse
+    {
+        /// <summary>
+        /// The external case ID that was sent in the request.
+        /// </summary>
+        public string ReferenceId { get; set; }
+
+        /// <summary>
+        /// The ID that uniquely identifies the case as it exists in Tethr.
+        /// </summary>
+        /// <remarks>
+        /// This is the primary ID for the case in Tethr, and wherever possible should be stored by the integrating system for troubleshooting purposes.
+        /// </remarks>
+        public string CaseId { get; set; }
+	
+        /// <summary>
+        /// The Collection ID this interaction is associated to.
+        /// </summary>
+        public string CollectionId { get; set; }
+    }
+}
---
+++ src/Tethr.AudioBroker/Model/CaseSession.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Tethr.AudioBroker.Model;
+
+namespace Tethr.AudioBroker
+{
+    /// <summary>
+    /// Request for creating a case.
+    /// </summary>
+    /// <example>
+    /// {"referenceId":"case1234","metadata":{"field__c":"a value"},"utcStart":"2017-08-16T19:30:00Z","utcEnd":"2017-08-16T19:35:00Z","messages":[{"senderReferenceId":"C1","utcTimestamp":"2017-08-16T19:30:00Z","content":"Hello, I need some help!","channel":"Web"}],"contacts":[{"referenceId":"C1","firstName":"Jane","lastName":"Doe","email":"jane@doe.org","phoneNumber":"4155551212","type":"customer"}]}
+    /// </example>
+    public class CaseSession
+    {
+        /// <summary>
+        /// The external case ID from the source system.
+        /// </summary>
+        public string ReferenceId { get; set; }
+        
+        /// <summary>
+        /// The master ID can be used to group cases together in a collection
+        /// to create a composite interaction. Analysis will generally treat
+        /// a composite interaction as a larger single interaction 
+        /// </summary>
+        public string MasterId { get; set; }
+        
+        /// <summary>
+        /// The Collection ID will group cases together, but not treat them
+        /// as a single large interaction for the purpose of analysis.
+        /// </summary>
+        public string CollectionId { get; set; }
+
+        /// <summary>
+        /// The metadata attached to the case.
+        /// </summary>
+        public JObject Metadata { get; set; }
+
+        /// <summary>
+        /// The datetime when the case was opened in UTC.
+        /// </summary>
+        public DateTime UtcStart { get; set; }
+		
+        /// <summary>
+        /// The datetime when the case was closed in UTC.
+        /// </summary>
+        public DateTime UtcEnd { get; set; }
+
+        /// <summary>
+        /// The messages for the case.
+        /// </summary>
+        public List<CaseMessage> Messages { get; set; }
+		
+        /// <summary>
+        /// The contacts that received the message.
+        /// </summary>
+        public List<CaseContact> Contacts { get; set; }
+    }
+}
